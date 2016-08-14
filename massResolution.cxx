af464d31d13fd9d1c067e0ce878a6f8eae56d8ab
Federico Massa <fedemassa91@gmail.com>
2016-08-14 03:22:23 +0200

    physics plots...

--- massResolution.cxx
+++ massResolution.cxx
@@ -14,12 +14,17 @@ void massResolution(const char* higgsFileName) {
 
   TFile* higgsFile= new TFile(higgsFileName);
 
-  
-  TH1F* onShell = (TH1F*) higgsFile->Get("EventHist_all__recoOnShellMass");
-  TH1F* offShell = (TH1F*) higgsFile->Get("EventHist_all__recoOffShellMass");
-  TH1F* tot = (TH1F*) higgsFile->Get("EventHist_all__recoMass");
+  TH1F* onShell27 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoOnShellMass27");
+  TH1F* offShell27 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoOffShellMass27");
+  TH1F* tot27 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoMass27");
 
+  TH1F* onShell32 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoOnShellMass32");
+  TH1F* offShell32 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoOffShellMass32");
+  TH1F* tot32 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoMass32");
 
+  TH1F* onShell4 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoOnShellMass4");
+  TH1F* offShell4 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoOffShellMass4");
+  TH1F* tot4 = (TH1F*) higgsFile->Get("EventHist_all__sigRecoMass4");
 
 
   gStyle->SetOptStat(0);
@@ -29,52 +34,115 @@ void massResolution(const char* higgsFileName) {
   gStyle->SetStatH(0.2);
 
 
-  plotStyle(onShell, "", 10, "m_{#mu#mu} [GeV]", "", kBlue, 0, 180); 
-  plotStyle(offShell, "", 10, "m_{#mu#mu} [GeV]", "", kBlue, 0, 180); 
-  plotStyle(tot, "", 10, "m_{4#mu} [GeV]", "", kBlue, 0, 180); 
+  plotStyle(onShell27, "", 10, "m_{#mu#mu} [GeV]", "", kBlack, -20, 20); 
+  plotStyle(onShell32, "", 10, "m_{#mu#mu} [GeV]", "", kRed, -20, 20); 
+  plotStyle(onShell4, "", 10, "m_{#mu#mu} [GeV]", "", kBlue, -20, 20); 
 
   ///////////////
 
   TCanvas* c = new TCanvas();
 
-  onShell->SetLineColor(kBlue);
   // offShell->SetFillColorAlpha(kRed,0.5);
   // offShell->SetFillStyle(3002);
-  onShell->SetTitle("");
-  onShell->GetYaxis()->SetTitle("Entries");
-  onShell->GetXaxis()->SetTitle("Reconstructed Z boson mass [GeV]");
-  onShell->Draw();
+  onShell27->SetTitle("");
+  onShell27->Scale(1/onShell27->GetEntries());
+  onShell27->GetYaxis()->SetTitle("Entries (normalized)");
+  onShell27->GetXaxis()->SetTitle("On-shell #mu#mu mass - truth on-shell Z mass [GeV]");
+  onShell27->GetYaxis()->SetRangeUser(0.0,0.16);
+  onShell27->Draw();
 
-  // onShell->SetFillColorAlpha(kBlue,0.5);
-  offShell->SetLineColor(kRed);
-  //  onShell->SetFillStyle(3004);
-  offShell->Draw("same");
+  onShell32->Scale(1/onShell32->GetEntries());
+  onShell32->Draw("same");
 
-  TLegend* leg = new TLegend(0.65,0.85,0.85,0.65);
-  leg->AddEntry(onShell, "on-shell Z", "lp");
-  leg->AddEntry(offShell, "off-shell Z", "lp");
+  onShell4->Scale(1/onShell4->GetEntries());
+  onShell4->Draw("same");
+
+
+  TLegend* leg = new TLegend(0.15,0.85,0.35,0.65);
+  leg->AddEntry(onShell27, "|#eta^{#mu}_{max}| < 2.7", "l");
+  leg->AddEntry(onShell32, "2.7 < |#eta^{#mu}_{max}| < 3.2", "l");
+  leg->AddEntry(onShell4, "3.2 < |#eta^{#mu}_{max}| < 4.0", "l");
 
   leg->Draw();
 
-  c->Print("recoOnShellVsOffShell.png","png");
+  c->Print("sigRecoOnShellMass.png","png");
 
   delete c;
   delete leg;
   /////////////////
 
+  plotStyle(offShell27, "", 10, "m_{#mu#mu} [GeV]", "", kBlack, -10, 10); 
+  plotStyle(offShell32, "", 10, "m_{#mu#mu} [GeV]", "", kRed, -10, 10); 
+  plotStyle(offShell4, "", 10, "m_{#mu#mu} [GeV]", "", kBlue, -10, 10); 
+
+
   c = new TCanvas();
+
+  // offShell->SetFillColorAlpha(kRed,0.5);
+  // offShell->SetFillStyle(3002);
+  offShell27->SetTitle("");
+  offShell27->Scale(1/offShell27->GetEntries());
+  offShell27->GetYaxis()->SetTitle("Entries (normalized)");
+  offShell27->GetXaxis()->SetTitle("Off-shell #mu#mu mass - truth off-shell Z mass [GeV]");
+  offShell27->GetYaxis()->SetRangeUser(0.0,0.18);
+  offShell27->Draw();
+
+  offShell32->Scale(1/offShell32->GetEntries());
+  offShell32->Draw("same");
+
+  offShell4->Scale(1/offShell4->GetEntries());
+  offShell4->Draw("same");
+
+
+  leg = new TLegend(0.15,0.85,0.35,0.65);
+  leg->AddEntry(offShell27, "|#eta^{#mu}_{max}| < 2.7", "l");
+  leg->AddEntry(offShell32, "2.7 < |#eta^{#mu}_{max}| < 3.2", "l");
+  leg->AddEntry(offShell4, "3.2 < |#eta^{#mu}_{max}| < 4.0", "l");
+
+  leg->Draw();
+
+  c->Print("sigRecoOffShellMass.png","png");
+
+  delete c;
+  delete leg;
   
-  
-  tot->SetLineColor(kGreen);
-  // tot->SetFillColorAlpha(kGreen-8,0.5);
+  ////////////===========
+
+  plotStyle(tot27, "", 10, "m_{4#mu} [GeV]", "", kBlack, -20, 20); 
+  plotStyle(tot32, "", 10, "m_{4#mu} [GeV]", "", kRed, -20, 20); 
+  plotStyle(tot4, "", 10, "m_{4#mu} [GeV]", "", kBlue, -20, 20); 
+
+
+  c = new TCanvas();
+
+  // tot->SetFillColorAlpha(kRed,0.5);
   // tot->SetFillStyle(3002);
-  tot->SetTitle("");
-  tot->GetYaxis()->SetTitle("Entries");
-  tot->GetXaxis()->SetTitle("Reconstructed four lepton mass [GeV]");
-  tot->GetXaxis()->SetRangeUser(40,170);
-  tot->Draw();
+  tot27->SetTitle("");
+  tot27->Scale(1/tot27->GetEntries());
+  tot27->GetYaxis()->SetTitle("Entries (normalized)");
+  tot27->GetXaxis()->SetTitle("4#mu mass - truth Higgs mass [GeV]");
+  tot27->GetYaxis()->SetRangeUser(0.0,0.16);
+  tot27->Draw();
+
+  tot32->Scale(1/tot32->GetEntries());
+  tot32->Draw("same");
+
+  tot4->Scale(1/tot4->GetEntries());
+  tot4->Draw("same");
+
+
+  leg = new TLegend(0.15,0.85,0.35,0.65);
+  leg->AddEntry(tot27, "|#eta^{#mu}_{max}| < 2.7", "l");
+  leg->AddEntry(tot32, "2.7 < |#eta^{#mu}_{max}| < 3.2", "l");
+  leg->AddEntry(tot4, "3.2 < |#eta^{#mu}_{max}| < 4.0", "l");
+
+  leg->Draw();
+
+  c->Print("sigRecoMass.png","png");
+
+  delete c;
+  delete leg;
   
-  c->Print("recoMass.png","png");
 
 
 }
---
+++ massResolutionLayout.cxx
@@ -0,0 +1,124 @@
+#include <TH1F.h>
+#include <TFile.h>
+#include <TLegend.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <utility>
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "plotStyle.h"
+
+void draw(std::vector<TH1F*>& v, const std::string& xaxis, const double yhigh, const std::string& imageName) {
+
+  TCanvas* c = new TCanvas();
+
+  if (v.size() != 3) {
+    std::cout << "DEVE ESSERE 3" << std::endl;
+    exit(1);
+  }
+
+  // offShell->SetFillColorAlpha(kRed,0.5);
+  // offShell->SetFillStyle(3002);
+  v[0]->SetTitle("");
+  v[0]->Scale(1/v[0]->GetEntries());
+  v[0]->GetYaxis()->SetTitle("Entries (normalized)");
+  v[0]->GetXaxis()->SetTitle(xaxis.c_str());
+  v[0]->GetYaxis()->SetRangeUser(0.0,yhigh);
+  v[0]->Draw();
+
+  v[1]->Scale(1/v[1]->GetEntries());
+  v[1]->Draw("same");
+
+  v[2]->Scale(1/v[2]->GetEntries());
+  v[2]->Draw("same");
+
+
+  TLegend* leg = new TLegend(0.15,0.85,0.35,0.65);
+  leg->AddEntry(v[0], "ExtBrl4", "l");
+  leg->AddEntry(v[1], "IExtBrl4", "l");
+  leg->AddEntry(v[2], "InclBrl4", "l");
+
+  leg->Draw();
+
+  c->Print((imageName+".png").c_str(),"png");
+
+  delete c;
+  delete leg;
+
+
+
+}
+
+void massResolution(const char* fname1, const char* fname2, const char* fname3) {
+
+  std::vector<TFile*> f;
+  f.push_back(new TFile(fname1));
+  f.push_back(new TFile(fname2));
+  f.push_back(new TFile(fname3));
+
+  std::vector<TH1F*> onShell27, offShell27, tot27,
+    onShell32, offShell32, tot32,
+    onShell4, offShell4, tot4;
+
+  for (int i = 0; i < 3; i++) {
+    onShell27.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoOnShellMass27"));
+    offShell27.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoOffShellMass27"));
+    tot27.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoMass27"));
+
+    onShell32.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoOnShellMass32"));
+    offShell32.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoOffShellMass32"));
+    tot32.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoMass32"));
+
+    onShell4.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoOnShellMass4"));
+    offShell4.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoOffShellMass4"));
+    tot4.push_back((TH1F*) f[i]->Get("EventHist_all__sigRecoMass4"));
+
+    
+    int color;
+    if (i == 0)
+      color = kRed;
+    else if (i == 1)
+      color = kGreen;
+    else if (i == 2)
+      color = kBlue;
+    
+    plotStyle(onShell27.back(), "", 10, "m_{#mu#mu} [GeV]", "", color, -20, 20); 
+    plotStyle(onShell32.back(), "", 10, "m_{#mu#mu} [GeV]", "", color, -20, 20); 
+    plotStyle(onShell4.back(), "", 10, "m_{#mu#mu} [GeV]", "", color, -20, 20); 
+    
+    plotStyle(offShell27.back(), "", 10, "m_{#mu#mu} [GeV]", "", color, -10, 10); 
+    plotStyle(offShell32.back(), "", 10, "m_{#mu#mu} [GeV]", "", color, -10, 10); 
+    plotStyle(offShell4.back(), "", 10, "m_{#mu#mu} [GeV]", "", color, -10, 10); 
+    
+    plotStyle(tot27.back(), "", 10, "m_{4#mu} [GeV]", "", color, -20, 20); 
+    plotStyle(tot32.back(), "", 10, "m_{4} [GeV]", "", color, -20, 20); 
+    plotStyle(tot4.back(), "", 10, "m_{4} [GeV]", "", color, -20, 20); 
+    
+
+  }
+
+
+  gStyle->SetOptStat(0);
+  gStyle->SetStatX(0.8);
+  gStyle->SetStatY(0.8);
+  gStyle->SetStatW(0.2);
+  gStyle->SetStatH(0.2);
+
+  draw(onShell27, "#mu#mu mass - truth on-shell Z boson mass [GeV]", 0.16, "sigRecoOnShell27");
+  draw(onShell32, "#mu#mu mass - truth on-shell Z boson mass [GeV]", 0.16, "sigRecoOnShell32");
+  draw(onShell4, "#mu#mu mass - truth on-shell Z boson mass [GeV]", 0.16, "sigRecoOnShell4");
+
+  draw(offShell27, "#mu#mu mass - truth off-shell Z boson mass [GeV]", 0.18, "sigRecoOffShell27");
+  draw(offShell32, "#mu#mu mass - truth off-shell Z boson mass [GeV]", 0.18, "sigRecoOffShell32");
+  draw(offShell4, "#mu#mu mass - truth off-shell Z boson mass [GeV]", 0.18, "sigRecoOffShell4");
+
+  draw(tot27, "4#mu mass - truth Higgs boson mass [GeV]", 0.16, "sigRecoOffShell27");
+  draw(tot32, "4#mu mass - truth Higgs boson mass [GeV]", 0.16, "sigRecoOffShell32");
+  draw(tot4, "4#mu mass - truth Higgs boson mass [GeV]", 0.16, "sigRecoOffShell4");
+
+
+
+
+}
